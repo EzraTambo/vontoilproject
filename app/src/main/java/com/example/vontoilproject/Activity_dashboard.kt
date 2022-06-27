fe6c79da25636ec737e8cb863956bf40b936c2ef
Ezra Tambo <Ezratash@gmail.com>
2022-06-27 22:18:55 +0300

    FINAL COMMIT

---
+++ app/src/main/java/com/example/vontoilproject/Activity_dashboard.kt
@@ -0,0 +1,15 @@
+package com.example.vontoilproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_dashboard : AppCompatActivity() {
+    fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard2)
+    }
+}
+
+open class AppCompatActivity {
+
+}
